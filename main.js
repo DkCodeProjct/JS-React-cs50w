c1c2729d8361865a66e944f556d6605f49a3b38c
DKcodeX <148317495+DkCodeProjct@users.noreply.github.com>
2024-04-11 18:24:21 +0530

    forLoop/i didnt watch tutorial

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+const rangeNum = 10 // constant value for num range
+
+for (let i=1; i <= rangeNum;  i+=1) { // i=1 while i <= 10 print(i;)
+    console.log(`Count:${i}`)
+};
+
+// js kind of cool, i wish i could learn it
+// but i did in cs50W web so dont get a chance to learn like python
+
+// js quit hard,, maybe it's caus i didnt learn it like from the basic{[cs50 covers only webSyntax]}
